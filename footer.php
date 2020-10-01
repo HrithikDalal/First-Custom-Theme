d5740740ec09d3db87a7bf138b35b31671866d60
Hrithik Dalal <hrithik.dalal@rtcamp.com>
2020-10-01 10:47:08 +0530

    Add Sass stylesheet and fixed img error

--- footer.php
+++ footer.php
@@ -8,11 +8,26 @@
 ?>
 </div>
 <footer class="site-footer">
-	<h2>Archives</h2>
-	<span> By Year - <?php wp_get_archives('type=yearly'); ?> </span>
-	<span> By Month - <?php wp_get_archives('type=monthly'); ?> </span>
-	<span> By Date - <?php wp_get_archives('type=daily'); ?> </span>
 
+	<!-- Archives -->
+	<h2>Archives</h2>
+	<!-- Yearly Archives -->
+	<select name="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+	<option value=""><?php echo attribute_escape(__('Select Year')); ?></option>
+	<?php wp_get_archives('type=yearly&format=option&show_post_count=1'); ?> </select>
+
+	<!-- Monthly Archives -->
+	<select name="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+	<option value=""><?php echo attribute_escape(__('Select Month')); ?></option>
+	<?php wp_get_archives('type=monthly&format=option&show_post_count=1'); ?> </select>
+
+	<!-- Daily Archives -->
+	<select name="archive-dropdown" onChange='document.location.href=this.options[this.selectedIndex].value;'>
+	<option value=""><?php echo attribute_escape(__('Select Date')); ?></option>
+	<?php wp_get_archives('type=daily&format=option&show_post_count=1'); ?> </select>
+
+
+	<!-- Sidebar -->
 	<?php if ( is_active_sidebar( 'sidebar-2' ) ) { ?>
 		<aside>
 			<?php dynamic_sidebar( 'sidebar-2' ); ?>
